f1c22de2e1f1419e47a4b245ac0d2e816e3a83d4
배으뜸 <baeeuddeum@baeeutteum-ui-MacBook-Pro.local>
2019-01-08 00:22:31 +0900

    v-7

---
+++ src/components/PhoneInfoList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PhoneInfo from './PhoneInfo';
+
+export default class PhoneInfoList extends Component {
+    static defaultProps = {
+        data: []
+    }
+
+    render(){
+        const { data } = this.props;
+        
+        
+        const list = data.map(
+            info => (<PhoneInfo key={info.id} info={info}/>)
+        );
+        /**
+         * const list = data.map(
+         *  (info, index) => (<PhoneInfo key={index} info={info}/>)
+         * );
+         */
+
+
+        return (
+            <div>
+                {list}
+            </div>
+        )
+    }
+}
